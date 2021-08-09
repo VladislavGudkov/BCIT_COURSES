a602402a466afb53a0552e26f89ca2f16950cda9
Vladislav Gudkov <36948186+VladislavGudkov@users.noreply.github.com>
2021-08-09 10:27:17 -0700

    Add files via upload

---
+++ COMP_1409 _Java_1/Assignment#3/Driver.java
@@ -0,0 +1,24 @@
+
+/**
+ * @author (Vladislav Gudkov)
+ * @version (November 29 2019)
+ */
+public class Driver
+{
+
+    public static void main(String[] args)
+    {
+
+        ParkedCar car1 = new ParkedCar("Vlad", "Ford", 1996, "A1B2C3", 150);
+        ParkedCar car2 = new ParkedCar("Vlad", "Ferrari", 2019, "Z9Y8X7", 140);
+
+        PoliceOfficer police1 = new PoliceOfficer ("Officer1", "111111");
+        PoliceOfficer police2 = new PoliceOfficer ("Officer2", "222222");
+
+        //police1.issueParkingTicket(car1, meter1);
+        //police2.issueParkingTic ket(car2, Parkingmeter.meter);
+
+    }
+}
+
+ 
