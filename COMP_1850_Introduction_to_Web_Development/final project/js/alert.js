47848df9c704afa43e5cf4b8df5ca1f7e60de2f0
Vladislav Gudkov <36948186+VladislavGudkov@users.noreply.github.com>
2021-08-09 11:11:14 -0700

    Add files via upload

---
+++ COMP_1850_Introduction_to_Web_Development/final project/js/alert.js
@@ -0,0 +1,3 @@
+function myFunction() {
+  alert("Details sent succesfully");
+}
---
+++ COMP_1850_Introduction_to_Web_Development/final project/js/confirmation.js
@@ -0,0 +1,9 @@
+function confimation() {
+  var txt;
+  if (confirm("Press a button!")) {
+    txt = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
